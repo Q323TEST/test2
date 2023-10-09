a3f33921ee9e2826dd777c1f0a044c04a25db502
AndrewSkoba <a.skoba89@gmail.com>
2023-10-09 22:53:12 +0300

    Test

---
+++ src/tt/Calculator.java
@@ -0,0 +1,19 @@
+package tt;
+
+public class Calculator {
+    static int addition(int num, int num2) {
+        return num + num2;
+    }
+
+    static int subtraction(int num, int num2) {
+        return num - num2;
+    }
+
+    static int multiplication(int num, int num2) {
+        return num * num2;
+    }
+
+    static int division(int num, int num2) {
+        return num / num2;
+    }
+}
